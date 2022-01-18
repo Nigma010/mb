ade270cba81eb3079298552163920d6119eccc2f
Nigma <rafanigmatulin22@gmail.com>
2022-01-19 02:25:14 +0300

    Initial commit

---
+++ posts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Posts
+
+class HomePageView(ListView):
+    model = Posts
+    template_name = 'home.html'
